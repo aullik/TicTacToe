ff2533cb2091b358a0f86598ee83c223044cd9ef
youidelh <youssef-wld@hotmail.fr>
2016-11-28 14:23:11 +0100

    adding the TODOS for backend

--- web/app/controllers/Application.scala
+++ web/app/controllers/Application.scala
@@ -10,17 +10,18 @@ class Application extends Controller {
   var controller: IController = null;
 
   def index = Action {
+
+    //TODO: the list of all users (username, email) must be send to the view
     val list = List("ysf", "nicolas", "dany", "ysf", "nicolas", "dany", "ysf", "nicolas", "dany", "ysf", "nicolas", "dany")
     Ok(bootstrap.views.html.index(list))
   }
 
-  /*
-    def tictactoe = Action {
-      // val controller = tictactoe.getController();
-      Ok(bootstrap.views.html.tictactoe(""))
-    }*/
+  def call(caller: String, calle: String) = Action {
+    //TODO: this is the method when a user want to play with another one
+    //TODO: hier will be the initialization on a controller and adding it the the list and setPlayers method
+    //TODO: will be called with the user names
 
-  def call(caller: String) = Action {
+    //this code has to be improved
     if (caller.equals("1")) {
       val tictactoe = new TicTacToe()
       controller = tictactoe.getController()
@@ -29,11 +30,28 @@ class Application extends Controller {
     Ok(bootstrap.views.html.tictactoe(controller.getStatus(), caller))
   }
 
-  def signup = Action {
+  def signupPage = Action {
+
+    //TODO: check if there is a coockie for user data if true to to index else go to signup
     Ok(bootstrap.views.html.signup())
   }
 
+  def signup = Action {
+    //TODO: save user in the users list and create an new user object
+    Ok("ok")
+  }
+
+  def login = Action {
+    //TODO: check if the data is from a current user if true go to the index page else return with errors.
+    Ok("ok")
+  }
+
   def move(data: String) = Action {
+
+    // TODO: you receive data as a String (grid-column-row) set a move and check if there is a win
+    // TODO: if true return status and win with 1 else return status with win with 0
+
+    //TODO: this code is to improve
     val list = data.split("-").map(_.toInt);
     controller.setValue(list(2), list(1), list(0))
     var returnedData = Map(
@@ -46,8 +64,6 @@ class Application extends Controller {
         "win" -> "0"
       )
     }
-
-
     var json = Json.toJson(returnedData)
     val jsonString: String = Json.stringify(json)
     Ok(jsonString);
