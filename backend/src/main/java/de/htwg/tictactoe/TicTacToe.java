7de3ef26496003696cb6e0d8738954ed9b209e85
aullik <nicolaswehrle@gmx.net>
2016-12-12 05:37:00 +0100

    fixed java Code

--- backend/src/main/java/de/htwg/tictactoe/TicTacToe.java
+++ backend/src/main/java/de/htwg/tictactoe/TicTacToe.java
@@ -2,46 +2,59 @@ package de.htwg.tictactoe;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Module;
 import de.htwg.tictactoe.controller.IController;
+import de.htwg.tictactoe.view.TextUI;
+import de.htwg.tictactoe.view.gui.TicTacToeGUI;
+import org.apache.log4j.PropertyConfigurator;
 
 import java.util.Scanner;
 
 public class TicTacToe {
 
-    static Scanner scanner;
-    String line = "";
+   private final IController controller;
+   private final Injector injector;
 
-    private static IController controller;
+   public TicTacToe() {
+      this(new TicTacToeModule());
+   }
 
-    public TicTacToe() {
-        Injector injector = Guice.createInjector(new TicTacToeModule());
-        controller = injector.getInstance(IController.class);
-    }
-
-    /*
-        public static void main(String[] args) {
-
-            // Set up logging through log4j
-            PropertyConfigurator.configure("log4j.properties");
+   public TicTacToe(final Module module) {
+      injector = Guice.createInjector(module);
+      controller = injector.getInstance(IController.class);
+   }
 
 
-            // Set up Google Guice Dependency Injector
+   void startUI() {
+      TextUI tui = injector.getInstance(TextUI.class);
+      injector.getInstance(TicTacToeGUI.class);
+
+      tui.printTUI();
+      //continue until the user decides to quit
+      boolean running = true;
+      Scanner scanner = new Scanner(System.in);
+      while (running) {
+         running = tui.processInputLine(scanner.next());
+      }
+   }
+
+   public static TicTacToe apply() {
+      return new TicTacToe(new TicTacToeModule());
+   }
+
+   public static void main(String[] args) {
+
+      // Set up logging through log4j
+      PropertyConfigurator.configure("log4j.properties");
 
 
-            TextUI tui = injector.getInstance(TextUI.class);
+      // Set up Google Guice Dependency Injector
+      TicTacToe ttt = new TicTacToe(new TicTacToeModule());
+      ttt.startUI();
 
-            injector.getInstance(TicTacToeGUI.class);
+   }
 
-            tui.printTUI();
-            //continue until the user decides to quit
-            boolean continu = true;
-            scanner = new Scanner(System.in);
-            while (continu) {
-                continu = tui.processInputLine(scanner.next());
-            }
-        }
-    */
-    public IController getController() {
-        return controller;
-    }
+   public IController getController() {
+      return controller;
+   }
 }
