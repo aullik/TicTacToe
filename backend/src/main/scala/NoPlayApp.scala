796ecdaace188507c6bdede4a64965f8f83a3df3
aullik <nicolaswehrle@gmx.net>
2017-01-14 19:42:18 +0100

    added NoPlayApp to work without a webserver

---
+++ backend/src/main/scala/NoPlayApp.scala
@@ -0,0 +1,25 @@
+import java.io.File
+
+import akka.actor.ActorSystem
+import com.google.inject.Guice
+import com.typesafe.config.ConfigFactory
+import net.codingwell.scalaguice.ScalaModule
+import tictactoe.TicTacToeModule
+
+/**
+  */
+object NoPlayApp extends App {
+
+  val conf = new File("web/conf/application.conf")
+
+  val system = ActorSystem("mySystem", ConfigFactory.parseFile(conf).resolve())
+  val injector = Guice.createInjector(new TicTacToeModule, new PlayMockModule(system))
+
+}
+
+class PlayMockModule(system: ActorSystem) extends ScalaModule {
+  override def configure(): Unit = {
+    bind[ActorSystem].toInstance(system)
+  }
+}
+
