67249d1696526581efe160bf8d97b079ba721f98
aullik <nicolaswehrle@gmx.net>
2017-01-20 10:46:28 +0100

    simplified AbstractAction.scala

--- web/app/controllers/authentication/AbstractAction.scala
+++ web/app/controllers/authentication/AbstractAction.scala
@@ -30,54 +30,51 @@ private[controllers] abstract class AbstractAction[REQ <: Request[AnyContent]](m
                                                     ec: ExecutionContext)
                                                    (request: Request[AnyContent]): Future[Result]
 
-  protected[authentication] def doAction(block: (REQ, () => Messages) => Result): Action[AnyContent] =
-    doActionFuture((r, m) => Future.successful(block(r, m)))
+  protected[authentication] def doAction(block: (REQ) => Result): Action[AnyContent] =
+    doActionFuture((r) => Future.successful(block(r)))
 
 
-  protected[authentication] def doActionFuture(block: (REQ, () => Messages) => Future[Result]): Action[AnyContent] =
-    Async.async(handleFutureRequest(buildBlock(block) _, Async.executionContext) _)
+  protected[authentication] def doActionFuture(block: (REQ) => Future[Result]): Action[AnyContent] =
+    Async.async(handleFutureRequest(block, Async.executionContext) _)
 
-  private def buildBlock(block: (REQ, () => Messages) => Future[Result])(r: REQ): Future[Result] = {
-    block(r, () => messagesApi.preferred(r))
-  }
 
   implicit class ResultFunction(val fnc: () => Result) {}
 
-  def apply(block: ResultFunction): Action[AnyContent] = doAction((r, m) => block.fnc())
+  def apply(block: ResultFunction): Action[AnyContent] = doAction((r) => block.fnc())
 
   implicit class RequestFunction(val fnc: (REQ) => Result) {}
 
-  def apply(block: RequestFunction): Action[AnyContent] = doAction((r, m) => block.fnc(r))
+  def apply(block: RequestFunction): Action[AnyContent] = doAction((r) => block.fnc(r))
 
   implicit class MessageFunction(val fnc: (REQ, Messages) => Result) {}
 
-  def apply(block: MessageFunction): Action[AnyContent] = doAction((r, m) => block.fnc(r, m()))
+  def apply(block: MessageFunction): Action[AnyContent] = doAction((r) => block.fnc(r, MSG(r)))
 
 
   implicit class FutureResultFunction(val fnc: () => Future[Result]) {}
 
-  def apply(block: FutureResultFunction): Action[AnyContent] = doActionFuture((r, m) => block.fnc())
+  def apply(block: FutureResultFunction): Action[AnyContent] = doActionFuture((r) => block.fnc())
 
   implicit class FutureRequestFunction(val fnc: (REQ) => Future[Result]) {}
 
-  def apply(block: FutureRequestFunction): Action[AnyContent] = doActionFuture((r, m) => block.fnc(r))
+  def apply(block: FutureRequestFunction): Action[AnyContent] = doActionFuture((r) => block.fnc(r))
 
   implicit class FutureMessageFunction(val fnc: (REQ, Messages) => Future[Result]) {}
 
-  def apply(block: FutureMessageFunction): Action[AnyContent] = doActionFuture((r, m) => block.fnc(r, m()))
+  def apply(block: FutureMessageFunction): Action[AnyContent] = doActionFuture((r) => block.fnc(r, MSG(r)))
 
 
   implicit class FutureECFunction(val fnc: (ExecutionContext) => Future[Result]) {}
 
-  def apply(block: FutureECFunction): Action[AnyContent] = doActionFuture((r, m) => block.fnc(Async.executionContext))
+  def apply(block: FutureECFunction): Action[AnyContent] = doActionFuture((r) => block.fnc(Async.executionContext))
 
   implicit class FutureRequestECFunction(val fnc: (REQ, ExecutionContext) => Future[Result]) {}
 
-  def apply(block: FutureRequestECFunction): Action[AnyContent] = doActionFuture((r, m) => block.fnc(r, Async.executionContext))
+  def apply(block: FutureRequestECFunction): Action[AnyContent] = doActionFuture((r) => block.fnc(r, Async.executionContext))
 
   implicit class FutureMessageECFunction(val fnc: (REQ, Messages, ExecutionContext) => Future[Result]) {}
 
-  def apply(block: FutureMessageECFunction): Action[AnyContent] = doActionFuture((r, m) => block.fnc(r, m(), Async.executionContext))
+  def apply(block: FutureMessageECFunction): Action[AnyContent] = doActionFuture((r) => block.fnc(r, MSG(r), Async.executionContext))
 
 
   protected object Async extends ActionBuilder[Request] {
@@ -86,5 +83,9 @@ private[controllers] abstract class AbstractAction[REQ <: Request[AnyContent]](m
     override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]) = block(request)
   }
 
+  private object MSG {
+    def apply(r: Request[AnyContent]) = messagesApi.preferred(r)
+  }
 
 }
+
