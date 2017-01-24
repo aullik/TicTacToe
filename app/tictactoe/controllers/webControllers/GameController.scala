19260df6b9a4baa2f3a6ceaf6109866ffae58fa3
aullik <nicolaswehrle@gmx.net>
2017-01-25 00:36:13 +0100

    removed legacyCode

--- app/tictactoe/controllers/webControllers/GameController.scala
+++ app/tictactoe/controllers/webControllers/GameController.scala
@@ -1,41 +1,14 @@
 package tictactoe.controllers.webControllers
 
 import com.mohiva.play.silhouette.api.actions.SecuredRequest
-import de.htwg.tictactoe.TicTacToe
-import de.htwg.tictactoe.controller.IController
 import framework.FrameworkSelector
-import play.api.libs.json.Json
-import play.api.mvc.Results._
-import play.api.mvc.{AnyContent, Request, Result}
+import play.api.mvc.{AnyContent, Result}
 import tictactoe.silhouette.TicTacToeEnv
-import tictactoe.model.User
-
-import scala.collection.mutable
 
 
 /**
   */
 object GameController {
-
-  private val cacheUserName2Game = mutable.Map.empty[String, TicTacToe]
-
-  /**
-    * Start the game internally.
-    *
-    * @param request
-    * @param otherPlayer
-    * @return
-    */
-  def startGame(otherPlayer: String)(user: User, request: Request[AnyContent]): Result = {
-    if (cacheUserName2Game.get(otherPlayer).isDefined || cacheUserName2Game.get(user.name).isDefined)
-      BadRequest("User already in game")
-
-    val tictactoe = new TicTacToe()
-    tictactoe.getController.setPlayers(user.name, otherPlayer)
-    cacheUserName2Game.put(user.name, tictactoe)
-    cacheUserName2Game.put(otherPlayer, tictactoe)
-    Ok("Game started")
-  }
 
   /**
     * connect to already started game. If no game is started connect to index
@@ -45,31 +18,6 @@ object GameController {
     */
   def game(request: SecuredRequest[TicTacToeEnv, AnyContent], fws: FrameworkSelector): Result = {
     fws.game
-  }
-
-
-  def move(user: User, data: String, request: Request[AnyContent]): Result = {
-    val controller: IController = null
-    // TODO: you receive data as a String (grid-column-row) set a move and check if there is a win
-    // TODO: if true return status and win with 1 else return status with win with 0
-
-    //TODO: this code is to improve
-    val list = data.split("-").map(_.toInt)
-    controller.setValue(list(2), list(1), list(0))
-    var returnedData = Map(
-      "status" -> controller.getStatus,
-      "win" -> "1"
-    )
-    if (!controller.getWin(0) && !controller.getWin(1)) {
-      returnedData = Map(
-        "status" -> controller.getStatus,
-        "win" -> "0"
-      )
-    }
-    val json = Json.toJson(returnedData)
-    val jsonString: String = Json.stringify(json)
-    Ok(jsonString)
-
   }
 
 
