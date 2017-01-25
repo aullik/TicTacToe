8d6e69b1f0e516b12e285f9b343d54f4557e5b8b
youidelh <youssef-wld@hotmail.fr>
2017-01-25 07:49:34 +0100

    game finish

--- app/assets/app/backToIndexModal.component.ts
+++ app/assets/app/backToIndexModal.component.ts
@@ -6,12 +6,11 @@ import {Component} from '@angular/core';
         <h1 md-dialog-title>Spiel ist fertig</h1>
             <div md-dialog-content>Zurück zu Index Page</div>
         <div md-dialog-actions>
-          <button md-button (click)="redirect">Ja</button>
+          <button md-button (click)="redirect()">Ja</button>
         </div>
     `,
 })
 export class BackToIndexModal {
-    public user: string;
     public redirect() {
         window.location.href = 'https://' + window.location.host + '/index';
     }
