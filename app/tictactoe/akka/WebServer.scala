3d7af073bb1125fa609b3be6be1758d502b3cf9b
youidelh <youssef.idelh@gmail.com>
2017-06-26 12:48:06 +0200

    Update WebServer.scala

--- app/tictactoe/akka/WebServer.scala
+++ app/tictactoe/akka/WebServer.scala
@@ -22,51 +22,9 @@ import tictactoe.persistence.{Persistence, PersistenceEnvironment, ProductivePer
 import tictactoe.persistence.databaseAccess.PersistenceAccessor
 import tictactoe.persistence.databaseAccess.PersistenceAccessor._
 import tictactoe.persistence.databaseAccess.slick.SlickPersistence
-//import tictactoe.actor.user.UserHandlerActor.lobbyCache
-//UserTokenManagerActor.TOKEN
 
 import scala.collection.mutable
 import scala.io.StdIn
-
-/*
-import java.io.File
-
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives._
-import akka.stream.ActorMaterializer
-import com.typesafe.config.ConfigFactory
-
-import scala.io.StdIn
-/**
-  * Created by Y on 24.05.2017.
-  */
-object WebServer {/*
-  def main(args: Array[String]) {
-
-    val conf = new File("web/conf/application.conf")
-    implicit val system = ActorSystem("systemTwo", ConfigFactory.parseFile(conf).resolve())
-    implicit val materializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
-    implicit val executionContext = system.dispatcher
-
-    val route =
-      path("users") {
-        get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
-        }
-      }
-
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 55)
-
-    println(s"Server online at http://localhost:55/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
-  }*/
-}*/
 
 
 
